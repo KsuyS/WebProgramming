06726f264b7b583833c9ebcb92fb510e3c912b6f
[KsuyS] <[solovevak105@gmail.com]>
2024-04-09 23:59:46 +0300

    Запросы на создание и заполнение базы данных

---
+++ data/sql/otherposts.sql
@@ -0,0 +1,13 @@
+INSERT INTO post (title, subtitle, image_post, author, publish_date, image_author, featured, class) VALUES ('From Top Down', 'Once a year, go someplace you’ve never been before.', 'http://localhost/images/flashlights.png', 'William Wong', '1443182400', 'http://localhost/images/william-wong.png', 1, 'features__block-article2');
+
+INSERT INTO post (title, subtitle, image_post, author, publish_date, image_author, featured) VALUES ('Still Standing Tall', 'Life begins at the end of your comfort zone.', 'http://localhost/images/balloons.png', 'William Wong', '1443182400', 'http://localhost/images/william-wong.png', 0);
+
+INSERT INTO post (title, subtitle, image_post, author, publish_date, image_author, featured) VALUES ('Sunny Side Up', 'No place is ever as bad as they tell you it’s going to be.', 'http://localhost/images/bridge.png', 'Mat Vogels', '1443182400', 'http://localhost/images/mat-vogels.png', 0);
+
+INSERT INTO post (title, subtitle, image_post, author, publish_date, image_author, featured) VALUES ('Water Falls', 'We travel not to escape life, but for life not to escape us.', 'http://localhost/images/river.png', 'Mat Vogels', '1443182400', 'http://localhost/images/mat-vogels.png', 0);
+
+INSERT INTO post (title, subtitle, image_post, author, publish_date, image_author, featured) VALUES ('Through the Mist', 'Travel makes you see what a tiny place you occupy in the world.', 'http://localhost/images/ocean.png', 'William Wong', '1443182400', 'http://localhost/images/william-wong.png', 0);
+
+INSERT INTO post (title, subtitle, image_post, author, publish_date, image_author, featured) VALUES ('Awaken Early', 'Not all those who wander are lost.', 'http://localhost/images/fog.png', 'Mat Vogels', '1443182400', 'http://localhost/images/mat-vogels.png', 0);
+
+INSERT INTO post (title, subtitle, image_post, author, publish_date, image_author, featured) VALUES ('Try it Always', 'The world is a book, and those who do not travel read only one page.', 'http://localhost/images/waterfall.png', 'Mat Vogels', '1443182400', 'http://localhost/images/mat-vogels.png', 0);
---
+++ data/sql/table.sql
@@ -0,0 +1,19 @@
+CREATE TABLE post
+(
+   `post_id`      INT NOT NULL AUTO_INCREMENT,
+   `title`        VARCHAR(255) NOT NULL,
+   `subtitle`     VARCHAR(255) NOT NULL,
+   `image_post`   VARCHAR(255) NOT NULL,
+   `content`      TEXT,
+   `author`       VARCHAR(255) NOT NULL,
+   `publish_date` VARCHAR(255) NOT NULL,
+   `author_url`   VARCHAR(255),
+   `image_author` VARCHAR(255),
+   `featured`     TINYINT(1) DEFAULT 0,
+   `class`        VARCHAR(255),
+   PRIMARY KEY (`post_id`)
+) ENGINE = InnoDB
+CHARACTER SET = utf8mb4
+COLLATE utf8mb4_unicode_ci
+;
+
